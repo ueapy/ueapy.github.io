0c2332c9e57fd3701b923a1b31c03fc875336c13
Dennis Sergeev <dennis.sergeev@gmail.com>
2016-10-05 18:55:42 +0100

    finish last weeks meeting summary

---
+++ content/2016-09-30-scripts-and-modules.md
@@ -0,0 +1,6 @@
+Title: Using Python scripts and modules 
+Author: Denis Sergeev
+date: 2016-09-30 17:54
+tags: basics
+
+{% notebook 2016-09-30-scripts-and-modules.ipynb cells[2:] %}
---
+++ content/notebooks/demo.py
@@ -0,0 +1,17 @@
+# A demo module
+
+
+def show_me_a():
+    """Prints a."""
+    print('a')
+
+def show_me_b():
+    """Prints b."""
+    print('b')
+
+c = 2
+d = 2
+
+
+if __name__ == '__main__':
+    show_me_a()
