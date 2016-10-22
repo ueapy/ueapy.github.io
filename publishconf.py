1e9dd48569ad4b7239e395cb7068d21643f440be
Dennis Sergeev <dennis.sergeev@gmail.com>
2016-10-22 12:08:42 +0100

    enforce https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ueapy.github.io'
+SITEURL = 'https://ueapy.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None #'feeds/all.atom.xml'
