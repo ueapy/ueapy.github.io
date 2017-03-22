49ff76d6093a5aabd877bb296b23b0485f2a2e45
Dennis Sergeev <dennis.sergeev@gmail.com>
2017-03-22 10:21:37 +0000

    fix previous commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,6 +13,7 @@ STATIC_PATHS = ['extra/robots.txt', 'pdfs', 'figures',
 EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': 'favicon.ico'},
     'extra/custom.css': {'path': 'extra/custom.css'}
+    'extra/robots.txt': {'path': 'extra/robots.txt'}
 }
 CUSTOM_CSS = 'extra/custom.css'
 
